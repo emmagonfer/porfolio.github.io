733877bf8684708d42ca7efc195e170447b8fd5b
emmagonfer <emmagfpc@gmail.com>
2026-04-21 11:26:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -135,9 +135,9 @@ document.addEventListener('DOMContentLoaded', () => {
             description: 'Estudio fotográfico centrado en capturar el dinamismo, la estela del tiempo y la sensación de velocidad.',
             program: 'CAMARA',
             images: ['../img/foto/3/primera foto.JPG',
-                '../img/foto/3/1.JPG',
                 '../img/foto/3/2.JPG',
                 '../img/foto/3/3.JPG',
+                '../img/foto/3/4.JPG',
             ]
         },
         'capturar-momento': {
@@ -211,7 +211,6 @@ document.addEventListener('DOMContentLoaded', () => {
             description: 'Se diseñaron 12 pictogramas sobre salud mental con una estética cuadrada, usando una retícula de líneas horizontales, verticales y a 45°. Las formas se construyeron principalmente con rectángulos y círculos, asegurando que todos encajaran en un mismo cuadrado base. Tras ajustar problemas de legibilidad en tamaños pequeños y en negativo, se definió el estilo final. Se eligió la tipografía Avenir por su coherencia geométrica y se propusieron aplicaciones como carteles, patrones y una app personalizada, buscando una función más allá de lo estético. Los pictogramas representan: narcisismo, miedo a hablar en público, insomnio, bipolaridad, problemas alimentarios, conciencia de la salud mental, depresión, autolesiones, enfado, ansiedad, esquizofrenia y miedo.',
             program: 'ILLUSTRATOR',
             images: ['../img/IDENTIDAD GRÁFICA/pictogramas salud mental/primera foto.jpg',
-                '../img/IDENTIDAD GRÁFICA/pictogramas salud mental/1.jpg',
                 '../img/IDENTIDAD GRÁFICA/pictogramas salud mental/2.jpg',
                 '../img/IDENTIDAD GRÁFICA/pictogramas salud mental/3.png',
                 '../img/IDENTIDAD GRÁFICA/pictogramas salud mental/4.png',
