a61e551f0c2a390193c4f1430ca2909114a3e208
Emma <emmagfpc@gmail.com>
2026-06-23 13:08:35 +0200

    arreglado el crasheo de javascript

--- assets/script.js
+++ assets/script.js
@@ -459,7 +459,6 @@ function updateModalContent() {
     });
 
 });
-<<<<<<< HEAD:assets/script.js
 
 
 gsap.set(".circulo-azul", { xPercent: -50, yPercent: -50 });
@@ -519,8 +518,3 @@ columnaDerecha.forEach((item) => {
         }
     });
 });
-<<<<<<< HEAD
-=======
->>>>>>> e39651f84cd83a401cf803b2baba7cd8978e617f:script.js
-=======
->>>>>>> 2dd5d32c560c0de6038aaebab79f30009f735707
